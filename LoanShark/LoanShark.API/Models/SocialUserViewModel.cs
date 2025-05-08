0db2b9c054a77de3dc5ff805902412a9e0bd3dc6
bebix <56645798+bebix@users.noreply.github.com>
2025-05-08 20:36:33 +0300

    fix viewmodel

--- LoanShark/LoanShark.API/Models/SocialUserViewModel.cs
+++ LoanShark/LoanShark.API/Models/SocialUserViewModel.cs
@@ -5,17 +5,14 @@ namespace LoanShark.API.Models
     public class SocialUserViewModel
     {
         public int UserID { get; set; }
-        public Cnp Cnp { get; set; }
+        public string Cnp { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Email Email { get; set; }
-        public PhoneNumber PhoneNumber { get; set; }
-        public HashedPassword HashedPassword { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string HashedPassword { get; set; }
         public string Username { get; set; }
         public int ReportedCount { get; set; }
-        public List<int> Friends { get; set; }
-        public List<int> Chats { get; set; }
-        private DateTime? TimeoutEnd { get; set; }
 
     }
 
