c2d9b5867bcc802106314801ccf7f1be99090e38
malinapetean <malinapetean@gmail.com>
2025-05-08 15:26:15 +0300

    Injections in program.cs

--- LoanShark/LoanShark.API/Program.cs
+++ LoanShark/LoanShark.API/Program.cs
@@ -1,5 +1,6 @@
 using LoanShark.EF.Repository.BankRepository;
 using LoanShark.Service.BankService;
+using LoanShark.Service.Service.BankService;
 using LoanShark.Web.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,6 +27,12 @@ namespace LoanShark.Web
             //Florin Transaction
             builder.Services.AddScoped<ITransactionsService, TransactionsService>();
             builder.Services.AddScoped<ITransactionsRepository, TransactionsRepositoryEF>();
+
+            //Maly Injections
+            builder.Services.AddScoped<IMainPageService, MainPageService>();
+            builder.Services.AddScoped<IMainPageRepository, MainPageRepositoryEF>();
+            builder.Services.AddScoped<ILoginService, LoginService>();
+            builder.Services.AddScoped<ILoginRepository, LoginRepositoryEF>();
 
             var app = builder.Build();
 
