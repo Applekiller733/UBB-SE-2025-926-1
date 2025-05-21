ab8bb3b96367687c7d08754024a477f62906abbd
916 Somesan Eduard <146818215+91SomesanEduard@users.noreply.github.com>
2025-05-21 18:10:00 +0300

    doamne ajuta

---
+++ LoanShark/LoanShark.MVC/Models/UserInformationViewModel.cs
@@ -0,0 +1,12 @@
+namespace LoanShark.MVC.Models
+{
+    public class UserInformationViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Cnp { get; set; }
+    }
+} 
