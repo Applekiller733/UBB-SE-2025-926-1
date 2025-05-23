3bdc3ccca380c92cf0441b5a3a694db420b73d66
916/1_Pop_Antonio <56645798+bebix@users.noreply.github.com>
2025-05-23 13:09:01 +0300

    FInal

--- LoanShark/LoanShark.MVC/Models/BankAccountEditModel.cs
+++ LoanShark/LoanShark.MVC/Models/BankAccountEditModel.cs
@@ -4,8 +4,9 @@
     {
         public string Iban { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-        public double DailyLimit { get; set; }
-        public double MaximumPerTransaction { get; set; }
+        public decimal DailyLimit { get; set; }
+        public decimal MaximumPerTransaction { get; set; }
+        public string Currency { get; set; }
         public int MaximumNrTransactions { get; set; }
         public bool IsBlocked { get; set; }
     }
