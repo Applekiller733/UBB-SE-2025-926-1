39aeb9a8d9884c6f8d05711a12f20a010d16402f
malinapetean <malinapetean@gmail.com>
2025-05-23 15:42:10 +0300

    More fixing...

--- LoanShark/LoanShark.Service/Service/SocialService/Implementations/UserService.cs
+++ LoanShark/LoanShark.Service/Service/SocialService/Implementations/UserService.cs
@@ -33,7 +33,7 @@ namespace LoanShark.Service.SocialService.Implementations
         {
             this.repo = repo;
             this.notificationService = notificationService;
-            this.userID = this.GetCurrentUser();
+            this.userID = 5;
         }
 
         /// <summary>
